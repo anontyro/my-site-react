44db44a117c382cadd10104f1338562d4d167bb1
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-11-11 18:11:07 +0800

    basics in place for the general iteration of the blog posts need to tidy up everything next

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -3,15 +3,33 @@ import { connect } from 'react-redux';
 
 import * as blogActions from '../reducers/actions/blogActions';
 
+function BlogItem(blog) {
+  console.log(blog.blog);
+  return (
+    <div className="blog-item">
+      <p>{blog.blog.title}</p>
+    </div>
+  );
+}
+
 class HomeView extends Component {
   componentDidMount() {
     this.props.getBlogList();
+  }
+
+  blogList() {
+    const list = [];
+    for (let i = 0; i < this.props.blog.length; i += 1) {
+      list.push(<BlogItem key={this.props.blog[i]._id} blog={this.props.blog[i]} />);
+    }
+    return list;
   }
 
   render() {
     return (
       <div className="home-main">
         <h1>HomePage</h1>
+        {this.blogList()}
       </div>
     );
   }
